cee6af845532bd86b65ad906860537a56fc9d818
hassan nawaz <hassannawazhiraj@outlook.com>
2017-12-16 14:44:31 +0500

    first commit

---
+++ src/jfacebook/jfacebook.java
@@ -0,0 +1,17 @@
+/*
+ * Made By : Hassan Nawaz
+ * All Rights Reserved
+ */
+package jfacebook;
+
+/**
+ *
+ * @author hassan
+ */
+public class jfacebook {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
