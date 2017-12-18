dd2f6cf005574b523dde97bc7bbcda21d7b24f18
hassan nawaz <hassannawazhiraj@outlook.com>
2017-12-18 21:45:23 +0500

    quick push

---
+++ src/fb/user.java
@@ -0,0 +1,18 @@
+/*
+ * Made By : Hassan Nawaz
+ * All Rights Reserved
+ */
+package fb;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hassan
+ */
+public class user {
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    
+}
